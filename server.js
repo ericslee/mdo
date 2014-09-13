8f46b788b6eaa7bb006fdac5f170cd8193b5f9b8
Eric Sun Lee <ericslee@seas.upenn.edu>
2014-09-12 23:17:18 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var server = http.createServer(app).listen(80); //the port you want to use
+console.log("Running server for MDO on port 80");
