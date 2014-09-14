81d8b4c5eeb2214bf36a176aa2c1bec8fa82cc4c
Brian Clanton <brian.clanton@gmail.com>
2014-09-13 22:37:12 -0400

    Added background image, started work on peer lobby.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@ $(document)
 	.ready(function() {
 		// Sidebar setup
 		$('.ui.sidebar')
-			.sidebar()
+			.sidebar('toggle')
+		;
+
+		$('#chat_send input')
+			.keypress(function(e) {
+				if (e.which === 13) {
+					// TODO: Send message to other peers
+					$(this).val("");
+				}
+			})
 		;
 	})
 ;
